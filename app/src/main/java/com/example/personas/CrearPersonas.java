5c92d40fa5c99b15e2baa3a1d9b9a8a32c6dd1e7
Michael Torres <michaeltorresberdugo@gmail.com>
2020-11-14 16:38:15 -0500

    Funcionalidad crear persona

--- app/src/main/java/com/example/personas/CrearPersonas.java
+++ app/src/main/java/com/example/personas/CrearPersonas.java
@@ -3,12 +3,44 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CrearPersonas extends AppCompatActivity {
+
+    private EditText cedula , nombre, apellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_personas);
+        cedula = findViewById(R.id.txtCedula);
+        nombre = findViewById(R.id.txtNombre);
+        apellido = findViewById(R.id.txtApellido);
+    }
+
+    public void guardar (View v){
+        String ced, nom,apell;
+        Persona p;
+
+        ced = cedula.getText().toString();
+        nom = nombre.getText().toString();
+        apell = apellido.getText().toString();
+        p = new Persona(ced,nom,apell);
+        p.guardar();
+        Toast.makeText(this, R.string.mensaje_guardado_exitoso , Toast.LENGTH_LONG).show();
+        limpiar();
+    }
+
+    public void limpiar (View v){
+        limpiar();
+    }
+
+    public void limpiar(){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
     }
 }

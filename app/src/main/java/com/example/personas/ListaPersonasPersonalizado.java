8c83e211f782e06c29daacec969a8e51c41ded77
Michael Torres <michaeltorresberdugo@gmail.com>
2020-11-20 18:49:14 -0500

    layout lista personalizada

---
+++ app/src/main/java/com/example/personas/ListaPersonasPersonalizado.java
@@ -0,0 +1,14 @@
+package com.example.personas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaPersonasPersonalizado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personas_personalizado);
+    }
+}
